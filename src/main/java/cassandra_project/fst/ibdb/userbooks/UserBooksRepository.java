79a89d800a28e26a8e4cc5f297002745379a9422
NasriHaithem <mai5haithem@gmail.com>
2021-11-02 15:18:41 +0100

    adding home page for authenticated user, adding rate/review functionalities

---
+++ src/main/java/cassandra_project/fst/ibdb/userbooks/UserBooksRepository.java
@@ -0,0 +1,9 @@
+package cassandra_project.fst.ibdb.userbooks;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserBooksRepository extends CassandraRepository<UserBooks, UserBooksPrimaryKey> {
+
+}
